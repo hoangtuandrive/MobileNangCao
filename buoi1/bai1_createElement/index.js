90e299e847e8ee9c2613362ab8376e93d8e1d357
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-09-01 12:57:23 +0700

    Done lab1

--- buoi1/bai1_createElement/index.js
+++ buoi1/bai1_createElement/index.js
@@ -2,19 +2,31 @@ document.body.onload = addElement;
 
 function addElement() {
     const a = document.createElement('input');
-    a.setAttribute('placeholder', 'input a');
+    a.setAttribute('placeholder', 'input a'); 
     document.body.appendChild(a);
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(document.createElement("br"));
 
     const b = document.createElement('input');
     b.setAttribute('placeholder', 'input b');
     document.body.appendChild(b);
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(document.createElement("br"));
 
     const button = document.createElement('button');
     button.innerHTML = 'Sum'
     document.body.appendChild(button);
+    document.body.appendChild(document.createElement("br"));
+    document.body.appendChild(document.createElement("br"));
+
+    const result = document.createElement('input');
+    result.setAttribute('placheholder','result');
+    document.body.appendChild(result);
 
     button.onclick = function() {
-        const a = parseInt(a.value);
-        const b = parseInt(b.value);
+        const avalue = parseInt(a.value);
+        const bvalue = parseInt(b.value);
+        console.log(avalue,bvalue,avalue+bvalue);
+        result.setAttribute('value', avalue + bvalue);
     }
 }
--- buoi1/bai3_fetch/fetch.js
+++ buoi1/bai3_fetch/fetch.js
@@ -1,3 +1,16 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((posts) => {
+        var html = "";
+        posts.forEach((post) => {
+          html += `<li>
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        </li>`;
+        });
+        var postBlock = document.getElementById("posts-block");
+        return (postBlock.innerHTML = html);
+      })
+    .catch((err) => {
+        console.log("Error");
+    });
